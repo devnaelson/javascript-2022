0b8a9f6cb9e6a2ca199581ff51ab1e430f851cd8
NaelsonBrasil <naelson.g.saraiva@gmail.com>
2022-01-25 08:21:10 -0400

    Up

--- Vue/main.js
+++ Vue/main.js
@@ -1,13 +1,22 @@
 const Counter = {
+
+    /*     data: function () {
+            return {
+                counter: 0
+            }, */
+
+    // es65 syntax
     data() {
         return {
             counter: 0
         }
-    }, mounted() {
+    }, mounted() { // if it is mount, then is mounted
         setInterval(() => {
             this.counter++
         }, 1000)
     }
 }
 
+//Declarative Rendering
+// declaratively render data to the DOM using straightforward template syntax:
 Vue.createApp(Counter).mount('#counter')
