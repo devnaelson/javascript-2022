8333d3e4dc259ad80c2e40d35b0e3cc16e1383fe
= <naelson.g.saraiva@gmail.com>
2022-02-23 14:06:09 -0400

    Up

--- reactJSDoc/src/components/AddUser.js
+++ reactJSDoc/src/components/AddUser.js
@@ -9,7 +9,11 @@ import {
 export default function AddUser() {
     return (
        <Form>
-           
+           <FormGroup>
+            <Label>Name</Label>
+            <Input type='text' placeholder='Enter'></Input>
+           </FormGroup>
+           <Button type='submit'>Submit</Button>
        </Form>
     );
 }
